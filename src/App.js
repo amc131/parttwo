06ca1fb6a8ff6a9bf5460553d363dbb1b3e26ea2
amc131 <76466256+amc131@users.noreply.github.com>
2022-06-21 20:14:00 -0700

    Updated for exercise 2.4

--- src/App.js
+++ src/App.js
@@ -1,25 +1,101 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+
+const Header = ({ course }) => {
+  return <h1>{course}</h1>
+}
+
+const Content = (parts) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {
+        parts.parts.map(
+          (element) => 
+          <Part key={element.id} part={element} />
+        )
+      }
     </div>
-  );
+  )
+} 
+
+const Part = (props) => {
+  const { part } = props
+
+  return (
+    <p>
+      {part.name} {part.exercises}
+    </p>
+  )
+}
+
+const Total = (props) => {
+  const total = props.exercises.reduce((s, p) => s + p) 
+  
+  return (
+    <p>
+      <b>total of {total} exercises</b>
+    </p>
+  )
+}
+        
+
+const App = () => {
+  const courses = [
+    {
+    id: 1,
+    name: 'Half Stack application development',
+    part: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      },
+      {
+        name: 'Redux',
+        exercises: 11,
+        id: 4
+      }
+    ]
+  },
+  {
+    name: 'Node.js',
+    id: 2,
+    part: [
+      {
+        name: 'Routing',
+        exercises: 3,
+        id: 1
+      },
+      {
+        name: 'Middlewares',
+        exercises: 7,
+        id: 2
+      }
+    ]
+  }
+]
+
+  return (
+    <div>
+      <Header course={courses[0].name} />
+      <Content parts={courses[0].part} />
+      <Total  exercises={courses[0].part.map(x => x['exercises'])} />
+      <Header course={courses[1].name} />
+      <Content parts={courses[1].part} />
+      <Total exercises={courses[1].part.map(x => x['exercises'])} />
+    </div>
+  )
 }
 
 export default App;
+
+
